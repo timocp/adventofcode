df8cbda0e4858b055a0bbfcb9b2b7393fa3c2e74
Tim Peters <tim@catminion.net>
2025-09-20 08:05:45 +0930

    Structure as multi-year

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,7 @@ use std::fmt;
 use std::io::Read;
 use std::time::Instant;
 
-mod day1;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day16;
-mod day17;
-mod day18;
-mod day19;
-mod day2;
-mod day20;
-mod day21;
-mod day22;
-mod day23;
-mod day24;
-mod day25;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
+mod y2015;
 
 #[derive(Eq, PartialEq)]
 pub enum Part {
@@ -46,56 +22,40 @@ impl fmt::Display for Part {
 fn main() {
     let args: Vec<_> = std::env::args().collect();
 
-    if args.len() == 2 {
-        run("2015", args[1].parse().unwrap());
-    } else {
+    if args.len() == 3 {
+        run(&args[1], args[2].parse().unwrap());
+    } else if args.len() == 2 {
         let t0 = Instant::now();
         for day in 1..=25 {
-            run("2015", day);
+            run(&args[1], day);
         }
         println!(
             "{:>80}",
             format!("TOTAL: {:.3}s", t0.elapsed().as_secs_f64())
         );
+    } else {
+        eprintln!("Usage: cargo run year [day]");
+        std::process::exit(1);
     }
 }
 
+type RunFn = fn(usize, &str, Part) -> String;
+
 fn run(year: &str, day: usize) {
+    let run: RunFn = match year {
+      "2015" => y2015::run,
+      _ => panic!("Unimplemented year: {}", year)
+    };
+
     let filename = format!("input/{}/day{}.txt", year, day);
+
     if let Ok(input) = read_file(&filename) {
         for part in [Part::One, Part::Two] {
-            print!("Day {:02}, part {}:  ", day, part);
+            print!("{} Day {:02}, part {}:  ", year, day, part);
             let t0 = Instant::now();
-            let result = match day {
-                1 => day1::run(&input, part),
-                2 => day2::run(&input, part),
-                3 => day3::run(&input, part),
-                4 => day4::run(&input, part),
-                5 => day5::run(&input, part),
-                6 => day6::run(&input, part),
-                7 => day7::run(&input, part),
-                8 => day8::run(&input, part),
-                9 => day9::run(&input, part),
-                10 => day10::run(&input, part),
-                11 => day11::run(&input, part),
-                12 => day12::run(&input, part),
-                13 => day13::run(&input, part),
-                14 => day14::run(&input, part),
-                15 => day15::run(&input, part),
-                16 => day16::run(&input, part),
-                17 => day17::run(&input, part),
-                18 => day18::run(&input, part),
-                19 => day19::run(&input, part),
-                20 => day20::run(&input, part),
-                21 => day21::run(&input, part),
-                22 => day22::run(&input, part),
-                23 => day23::run(&input, part),
-                24 => day24::run(&input, part),
-                25 => day25::run(&input, part),
-                _ => "Not implemented".to_string(),
-            };
+            let result = run(day, &input, part);
             println!(
-                "{:56} {1:.3}s",
+                "{:51} {1:.3}s",
                 if result.contains('\n') {
                     result.lines().next().unwrap()
                 } else {
@@ -105,7 +65,7 @@ fn run(year: &str, day: usize) {
             );
             if result.contains('\n') {
                 for line in result.lines().skip(1) {
-                    println!("{:17}{}", "", line);
+                    println!("{:21}{}", "", line);
                 }
             }
         }
---
+++ src/y2015.rs
@@ -0,0 +1,56 @@
+mod day1;
+mod day10;
+mod day11;
+mod day12;
+mod day13;
+mod day14;
+mod day15;
+mod day16;
+mod day17;
+mod day18;
+mod day19;
+mod day2;
+mod day20;
+mod day21;
+mod day22;
+mod day23;
+mod day24;
+mod day25;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+
+pub fn run(day: usize, input: &str, part: crate::Part) -> String {
+    match day {
+        1 => day1::run(input, part),
+        2 => day2::run(input, part),
+        3 => day3::run(input, part),
+        4 => day4::run(input, part),
+        5 => day5::run(input, part),
+        6 => day6::run(input, part),
+        7 => day7::run(input, part),
+        8 => day8::run(input, part),
+        9 => day9::run(input, part),
+        10 => day10::run(input, part),
+        11 => day11::run(input, part),
+        12 => day12::run(input, part),
+        13 => day13::run(input, part),
+        14 => day14::run(input, part),
+        15 => day15::run(input, part),
+        16 => day16::run(input, part),
+        17 => day17::run(input, part),
+        18 => day18::run(input, part),
+        19 => day19::run(input, part),
+        20 => day20::run(input, part),
+        21 => day21::run(input, part),
+        22 => day22::run(input, part),
+        23 => day23::run(input, part),
+        24 => day24::run(input, part),
+        25 => day25::run(input, part),
+        _ => "Not implemented".to_string(),
+    }
+}
