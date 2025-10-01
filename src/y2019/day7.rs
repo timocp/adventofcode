ece7098e79924096f176c07108cee7d96bbe6301
Tim Peters <tim@catminion.net>
2025-10-01 21:36:26 +0930

    Add 2019 day 7 part 2

--- src/y2019/day7.rs
+++ src/y2019/day7.rs
@@ -17,7 +17,7 @@ impl crate::Puzzle for Solver {
     }
 
     fn part2(&self) -> String {
-        "unimplemented".to_string()
+        maximise_feedback(&self.vm).to_string()
     }
 }
 
@@ -35,10 +35,34 @@ fn maximise_amps(vm: &Vm) -> i32 {
         .unwrap()
 }
 
+fn run(vm: &mut Vm, inputs: &[i32]) -> i32 {
+    *vm.run(inputs).first().unwrap()
+}
+
 fn amp(vm: &Vm, phase_setting: i32, input_signal: i32) -> i32 {
-    *vm.clone()
-        .run(&[phase_setting, input_signal])
-        .first()
+    run(&mut vm.clone(), &[phase_setting, input_signal])
+}
+
+fn maximise_feedback(vm: &Vm) -> i32 {
+    (5..10)
+        .permutations(5)
+        .map(|phase| {
+            let mut amps = vec![vm.clone(), vm.clone(), vm.clone(), vm.clone(), vm.clone()];
+            let mut signal = run(&mut amps[0], &[phase[0], 0]);
+            signal = run(&mut amps[1], &[phase[1], signal]);
+            signal = run(&mut amps[2], &[phase[2], signal]);
+            signal = run(&mut amps[3], &[phase[3], signal]);
+            signal = run(&mut amps[4], &[phase[4], signal]);
+            while !amps[4].is_halted() {
+                signal = run(&mut amps[0], &[signal]);
+                signal = run(&mut amps[1], &[signal]);
+                signal = run(&mut amps[2], &[signal]);
+                signal = run(&mut amps[3], &[signal]);
+                signal = run(&mut amps[4], &[signal]);
+            }
+            signal
+        })
+        .max()
         .unwrap()
 }
 
@@ -50,6 +74,21 @@ fn test_maximise_amps() {
     let vm = Vm::from("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0");
     assert_eq!(54321, maximise_amps(&vm));
 
-    let vm = Vm::from("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0");
+    let vm = Vm::from(
+        "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0",
+    );
     assert_eq!(65210, maximise_amps(&vm));
+}
+
+#[test]
+fn test_maximise_feedback() {
+    let vm = Vm::from(
+        "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5",
+    );
+    assert_eq!(139629729, maximise_feedback(&vm));
+
+    let vm = Vm::from(
+        "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10",
+    );
+    assert_eq!(18216, maximise_feedback(&vm));
 }
