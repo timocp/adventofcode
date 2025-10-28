678228101e338597d0eefe62db07c5e6b878d94b
Tim Peters <tim@catminion.net>
2025-10-28 21:14:38 +1030

    2019 day 16 part 2

--- src/y2019/day16.rs
+++ src/y2019/day16.rs
@@ -14,7 +14,7 @@ impl crate::Puzzle for Solver {
     }
 
     fn part2(&self) -> String {
-        "unimplemented".to_string()
+        real_message(&self.input_signal).to_string()
     }
 }
 
@@ -74,6 +74,33 @@ fn phase(input: &[i8], output: &mut [i8]) {
     }
 }
 
+// apply 100 phases on the large ("real") signal
+// signal is modified in-place during processing
+// returns the 8-digit message found at the offset
+fn real_message(input: &[i8]) -> i32 {
+    // generate the "real" signal
+    let mut signal = input.repeat(10000);
+
+    let offset = number_from_slice(&signal[0..7]) as usize;
+
+    // if offset > half the input length, all multiplies are 1
+    // we don't need to calculate any digits before the offset
+    for _ in 0..100 {
+        let mut acc = 0i32;
+        for i in (offset..(signal.len() - 1)).rev() {
+            acc += signal[i] as i32;
+            signal[i] = (acc.abs() % 10) as i8;
+        }
+    }
+
+    number_from_slice(&signal[offset..(offset + 8)])
+}
+
+fn number_from_slice(data: &[i8]) -> i32 {
+    data.iter()
+        .fold(0i32, |acc, digit| acc * 10 + *digit as i32)
+}
+
 #[test]
 fn test_phase() {
     let mut buf1 = vec![1, 2, 3, 4, 5, 6, 7, 8];
@@ -108,4 +135,16 @@ fn test_repeated_phase() {
             "69317163492948606335995924319873"
         )))
     );
+}
+
+#[test]
+fn test_real_message() {
+    let example1 = "03036732577212944063491565474664";
+    assert_eq!(84462026, real_message(&parse_input(example1)));
+
+    let example2 = "02935109699940807407585447034323";
+    assert_eq!(78725270, real_message(&parse_input(example2)));
+
+    let example3 = "03081770884921959731165446850517";
+    assert_eq!(53553731, real_message(&parse_input(example3)));
 }
