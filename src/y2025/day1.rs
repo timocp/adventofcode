ef8a0225df45c448f3baad6e91db93ff68b22f42
Tim Peters <tim@catminion.net>
2025-12-09 20:44:21 +1030

    2025 day 1 part 2

--- src/y2025/day1.rs
+++ src/y2025/day1.rs
@@ -10,11 +10,11 @@ impl crate::Puzzle for Solver {
     }
 
     fn part1(&self) -> String {
-        count_zeros(&self.input).to_string()
+        count_zeros(&self.input, false).to_string()
     }
 
     fn part2(&self) -> String {
-        "unimplemented".to_string()
+        count_zeros(&self.input, true).to_string()
     }
 }
 
@@ -24,17 +24,32 @@ enum Turn {
     Right(i32),
 }
 
-fn count_zeros(turns: &[Turn]) -> usize {
+fn count_zeros(turns: &[Turn], all: bool) -> i32 {
     let mut pos = 50;
     let mut count = 0;
     for turn in turns {
+        let started_at_zero = pos == 0;
         match turn {
             Turn::Left(i) => pos -= i,
             Turn::Right(i) => pos += i,
-        }
-        pos %= 100;
-        if pos == 0 {
+        };
+
+        let q = pos / 100;
+        if all {
+            match (started_at_zero, pos) {
+                (true, ..0) => count += -q,
+                (true, 100..) => count += q,
+                (false, ..=0) => count += -q + 1,
+                (false, 100..) => count += q,
+                (_, _) => {}
+            }
+        } else if pos % 100 == 0 {
             count += 1;
+        }
+
+        pos %= 100;
+        if pos < 0 {
+            pos += 100;
         }
     }
     count
@@ -70,5 +85,11 @@ R14
 L82
 ";
     let turns = parse_input(test_input);
-    assert_eq!(3, count_zeros(&turns));
+    assert_eq!(3, count_zeros(&turns, false));
+    assert_eq!(6, count_zeros(&turns, true));
+
+    assert_eq!(10, count_zeros(&parse_input("R1000\n"), true));
+    assert_eq!(10, count_zeros(&parse_input("L1000\n"), true));
+    assert_eq!(1, count_zeros(&parse_input("L50\nR1\n"), true));
+    assert_eq!(1, count_zeros(&parse_input("R50\nL1\n"), true));
 }
