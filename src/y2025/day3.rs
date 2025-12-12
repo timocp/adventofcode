526b6a5860be901bd6cd2290cca278d094dc49cf
Tim Peters <tim@catminion.net>
2025-12-13 07:49:24 +1030

    2025 day 3 part 2

--- src/y2025/day3.rs
+++ src/y2025/day3.rs
@@ -1,54 +1,48 @@
 pub struct Solver {
-    input: Vec<Vec<u32>>,
+    banks: Vec<Vec<u64>>,
 }
 
 impl crate::Puzzle for Solver {
     fn new(input: &str) -> Self {
         Self {
-            input: parse_input(input),
+            banks: parse_input(input),
         }
     }
 
     fn part1(&self) -> String {
-        sum_best_joltage(&self.input).to_string()
+        sum_best_joltage(&self.banks, 2).to_string()
     }
 
     fn part2(&self) -> String {
-        "unimplemented".to_string()
+        sum_best_joltage(&self.banks, 12).to_string()
     }
 }
 
-fn sum_best_joltage(banks: &[Vec<u32>]) -> u32 {
-    banks.iter().map(|batteries| best_joltage(batteries)).sum()
+fn sum_best_joltage(banks: &[Vec<u64>], digits: usize) -> u64 {
+    banks.iter().map(|bank| best_joltage(bank, digits)).sum()
 }
 
-fn best_joltage(bank: &[u32]) -> u32 {
-    for d1 in (1..=9).rev() {
-        if let Some(pos) = bank.iter().position(|&n| n == d1) {
-            // d1 is best first digit. what is the best next digit to its right?
-            if let Some(d2) = max_in(&bank[(pos + 1)..]) {
-                return d1 * 10 + d2;
-            }
+fn best_joltage(bank: &[u64], digits: usize) -> u64 {
+    let mut best = 0;
+    let mut best_at = 0;
+    for (i, battery) in bank.iter().enumerate().take(bank.len() - digits + 1) {
+        if *battery > best {
+            best = *battery;
+            best_at = i;
         }
     }
-    panic!("no solution in {:?}", bank);
-}
-
-fn max_in(batteries: &[u32]) -> Option<u32> {
-    let mut max: Option<u32> = None;
-    for d2 in batteries {
-        max = Some(match max {
-            None => *d2,
-            Some(i) => i.max(*d2),
-        });
+    if digits > 1 {
+        best * 10u64.pow((digits - 1).try_into().unwrap())
+            + best_joltage(&bank[(best_at + 1)..], digits - 1)
+    } else {
+        best
     }
-    max
 }
 
-fn parse_input(input: &str) -> Vec<Vec<u32>> {
+fn parse_input(input: &str) -> Vec<Vec<u64>> {
     input
         .lines()
-        .map(|line| line.chars().map(|c| c as u32 - 48).collect())
+        .map(|line| line.chars().map(|c| c as u64 - 48).collect())
         .collect()
 }
 
@@ -60,11 +54,17 @@ fn test() {
 234234234234278
 818181911112111
 ";
-    let batteries = parse_input(test_input);
+    let banks = parse_input(test_input);
 
-    assert_eq!(98, best_joltage(&batteries[0]));
-    assert_eq!(89, best_joltage(&batteries[1]));
-    assert_eq!(78, best_joltage(&batteries[2]));
-    assert_eq!(92, best_joltage(&batteries[3]));
-    assert_eq!(17346, sum_best_joltage(&batteries));
+    assert_eq!(98, best_joltage(&banks[0], 2));
+    assert_eq!(89, best_joltage(&banks[1], 2));
+    assert_eq!(78, best_joltage(&banks[2], 2));
+    assert_eq!(92, best_joltage(&banks[3], 2));
+    assert_eq!(357, sum_best_joltage(&banks, 2));
+
+    assert_eq!(987654321111, best_joltage(&banks[0], 12));
+    assert_eq!(811111111119, best_joltage(&banks[1], 12));
+    assert_eq!(434234234278, best_joltage(&banks[2], 12));
+    assert_eq!(888911112111, best_joltage(&banks[3], 12));
+    assert_eq!(3121910778619, sum_best_joltage(&banks, 12));
 }
