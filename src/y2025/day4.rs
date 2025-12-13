45d6dd45ebaf4fa077daeced0c41a7dc8fe6010e
Tim Peters <tim@catminion.net>
2025-12-13 14:29:52 +1030

    2025 day 4 part 2

--- src/y2025/day4.rs
+++ src/y2025/day4.rs
@@ -1,4 +1,4 @@
-use crate::grid::{ALL_DIRS, Grid};
+use crate::grid::{ALL_DIRS, Grid, Pos};
 
 pub struct Solver {
     grid: Grid<Cell>,
@@ -12,16 +12,16 @@ impl crate::Puzzle for Solver {
     }
 
     fn part1(&self) -> String {
-        accessible_rolls(&self.grid).to_string()
+        accessible_paper(&self.grid).len().to_string()
     }
 
     fn part2(&self) -> String {
-        "unimplemented".to_string()
+        total_removeable_paper(&self.grid).to_string()
     }
 }
 
-fn accessible_rolls(grid: &Grid<Cell>) -> u32 {
-    let mut count = 0;
+fn accessible_paper(grid: &Grid<Cell>) -> Vec<Pos> {
+    let mut positions = vec![];
     for (pos, cell) in grid.iter() {
         if matches!(cell, Cell::Paper)
             && ALL_DIRS
@@ -30,10 +30,31 @@ fn accessible_rolls(grid: &Grid<Cell>) -> u32 {
                 .count()
                 < 4
         {
-            count += 1;
+            positions.push(pos);
         }
     }
-    count
+    positions
+}
+
+// Removes accesible paper from the grid, returning the number of rolls that were removed
+fn remove_paper(grid: &mut Grid<Cell>) -> usize {
+    let accessible = accessible_paper(grid);
+    for p in &accessible {
+        grid.set(*p, Cell::Empty);
+    }
+    accessible.len()
+}
+
+fn total_removeable_paper(grid: &Grid<Cell>) -> usize {
+    let mut grid = grid.clone();
+    let mut total = 0;
+    loop {
+        let removed = remove_paper(&mut grid);
+        if removed == 0 {
+            return total;
+        }
+        total += removed;
+    }
 }
 
 fn parse_input(input: &str) -> Grid<Cell> {
@@ -71,5 +92,6 @@ fn test() {
 @.@.@@@.@.
 ";
     let grid = parse_input(test_input);
-    assert_eq!(13, accessible_rolls(&grid));
+    assert_eq!(13, accessible_paper(&grid).len());
+    assert_eq!(43, total_removeable_paper(&grid));
 }
